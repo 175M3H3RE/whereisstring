c9bbe29d3071c100e78d0403fbe16fa3a3983bed
175M3H3RE <117502862+175M3H3RE@users.noreply.github.com>
2023-10-16 14:10:44 +0530

    Update whereisit.bat

--- whereisit.bat
+++ whereisit.bat
@@ -1,5 +1,5 @@
 @echo off
-if ""%2=="" if "%1"=="/?" echo.Syntax--   [ScriptName] "String_here" "[Single Character C]"&echo.           C-- Character to Find&echo.Result--   Index location of character in String (starting from 1)
+if ""%2=="" if "%1"=="/?" echo.Syntax--   [ScriptName] "String_here" "[Single Character C]"&echo.           C-- Character to Find&echo.Result--   Index location of character in String (starting from 0)
 setlocal enabledelayedexpansion
 set str=%1
 set char=%2
